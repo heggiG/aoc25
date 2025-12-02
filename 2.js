ab2645a0226cd7934c72bc7edbfce8643a916abf
fheck <florian.heck@promatis.de>
2025-12-02 12:49:18 +0100

    tag 1 & 2

---
+++ 2.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', (line) => {
+    console.log('start');
+    let inputs = line.split(',');
+    let part1 = 0;
+    let part2 = 0;
+    for (let range of inputs) {
+        let ranges = range.split("-")
+        Array.from({ length: (Number(ranges[1]) + 1) - Number(ranges[0]) }, (x, i) => "" + (i + Number(ranges[0]))).forEach((num) => {
+            let match = /^([0-9]+)(\1)+$/.exec(num);
+            if (!!match) {
+                if (match) {
+                    part1 += Number(num);
+                }
+                part2 += Number(num);
+                return;
+            }
+        })
+        break;
+    }
+    console.log(part1)
+    console.log(part2)
+})
