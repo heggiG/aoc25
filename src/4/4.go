012ee694a98ab9a09c22d1b4efa9b23c45dc5726
fheck <florian.heck@promatis.de>
2025-12-04 14:58:13 +0100

    solution in go

--- src/4/4.go
+++ src/4/4.go
@@ -8,11 +8,83 @@ import (
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
-	sum1 := 0
-	sum2 := 0
+	sum := 0
+	sumOld := -1
+	var arr [][]rune
+	counter := 0
 	for input.Scan() {
-		
+		line := input.Text()
+		if (arr == nil) {
+			arr = make([][]rune, len(line))
+		}
+		toSet := make([]rune, 140)
+		for index, runeval := range line {
+			toSet[index] = runeval
+		}
+		arr[counter] = toSet
+		counter++
 	}
-	fmt.Println(sum1, sum2)
+	fmt.Println("Go!")
+	for sum - sumOld > 0 {
+		sumOld = sum
+		newarray := make([][]rune, len(arr))
+		for i := 0; i < 140; i++ {
+			lineNew := make([]rune, len(arr))
+			for j := 0; j < 140; j++ {
+				el := arr[i][j]
+				if el == '@' {
+					amount := 0
+					if access(&arr, i-1, j-1) == '@' {
+						amount++
+					}
+					if access(&arr, i, j-1) == '@' {
+						amount++
+					}
+					if access(&arr, i-1, j) == '@' {
+						amount++
+					}
+					if access(&arr, i+1, j) == '@' {
+						amount++
+					}
+					if access(&arr, i, j+1) == '@' {
+						amount++
+					}
+					if access(&arr, i+1, j+1) == '@' {
+						amount++
+					}
+					if access(&arr, i+1, j-1) == '@' {
+						amount++
+					}
+					if access(&arr, i-1, j+1) == '@' {
+						amount++
+					}
+					if amount >= 4 {
+						lineNew[j] = '@'
+						continue
+					}
+					sum++
+					lineNew[j] = '.'
+					continue
+				}
+				lineNew[j] = el
+
+			}
+			newarray[i] = lineNew
+		}
+		if sumOld == 0 {
+			fmt.Println(sum)
+		}
+		arr = newarray
+	}
+	fmt.Println(sum)
 }
 
+func access(arr *[][]rune, i int, j int) rune {
+	if i < 0 || i >= len(*arr) {
+		return '.'
+	}
+	if j < 0 || j >= len((*arr)[0]) {
+		return '.'
+	}
+	return (*arr)[i][j]
+}
