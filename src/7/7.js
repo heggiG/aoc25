547e209a4aa8c15c4b3ea1b50834ea2183b8224b
fheck <florian.heck@promatis.de>
2025-12-07 16:21:32 +0100

    cleanup

--- src/7/7.js
+++ src/7/7.js
@@ -7,28 +7,6 @@ const rl = readline.createInterface({
 });
 
 let lines = []
-String.prototype.replaceAt = function (index, replacement) {
-    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-}
-
-function pascals(numRows) {
-    if (numRows === 0) return [];
-    if (numRows === 1) return [[1]];
-    let result = [];
-    for (let row = 1; row <= numRows; row++) {
-        let arr = [];
-        for (let col = 0; col < row; col++) {
-            if (col === 0 || col === row - 1) {
-                arr.push(1);
-            } else {
-                arr.push((result[row - 2][col - 1] + result[row - 2][col]));
-            }
-        }
-        result.push(arr);
-    }
-    return result;
-}
-
 let sums = []
 
 console.log("Go!")
