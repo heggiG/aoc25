d014c122966ef2b0cb12971f604ea2b0ad5d252a
fheck <florian.heck@promatis.de>
2025-12-08 17:34:28 +0100

    less makros

--- src/7/7.c
+++ src/7/7.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define N 141
-#define M (N + 1)
-#define HALF (N >> 1)
-#define SPLIT '^'
 
 static long long sums[N];
 
-int main()
-{
-    char **input;
-    char *line = NULL;
+int main() {
+    const int M = N + 1;
+    const int HALF = N >> 1;
+    const char SPLIT = '^';
+    
+    char *line;
     size_t size;
     int i = 0;
     int splits = 0;
     sums[HALF] = 1;
     int col = HALF, end = HALF + 1;
+    printf("Go!\n");
     while (getline(&line, &size, stdin) > 0) {
         if (i % 2 == 1 || i == 0) {
             i++;
@@ -38,6 +39,7 @@ int main()
     for (int j = 0; j < N; j++) {
         worlds += sums[j];
     }
-    printf("%d %lld", splits, worlds);
+    printf("%d\n%lld\n", splits, worlds);
+    exit(0);
 }
 
