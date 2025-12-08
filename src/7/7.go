4e3b26f4adc22e0862dc15d0236bbeb7bbebd6b9
fheck <florian.heck@promatis.de>
2025-12-08 15:55:02 +0100

    better day 7 solution

--- src/7/7.go
+++ src/7/7.go
@@ -8,43 +8,36 @@ import (
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
-	var lines [][]rune
-	var sums [][]int
+	var lines []string
+	var sums []int
 	i := 0
 	for input.Scan() {
 		line := input.Text()
 		if lines == nil {
-			lines = make([][]rune, len(line)+1)
-			sums = make([][]int, len(line)+1)
+			lines = make([]string, len(line)+1)
+			sums = make([]int, len(line)+1)
 		}
-		lines[i] = []rune(line)
-		sums[i] = make([]int, len(line))
+		lines[i] = line
 		i++
 	}
 	sum := 0
 	sum2 := 0
-	for i, line := range lines[:len(lines)-1] {
-		for j, el := range line {
-			if el == 'S' {
-				lines[i+1][j] = '|'
-				sums[i+1][j] = 1
-				break
-			}
-			if el == '|' {
-				if lines[i+1][j] == '^' {
-					sum++
-					lines[i+1][j-1] = '|'
-					lines[i+1][j+1] = '|'
-					sums[i+1][j-1] = sums[i+1][j-1] + sums[i][j]
-					sums[i+1][j+1] = sums[i+1][j+1] + sums[i][j]
-				} else {
-					lines[i+1][j] = lines[i][j]
-					sums[i+1][j] = sums[i+1][j] + sums[i][j]
-				}
+	col := (len(lines) / 2) - 1
+	end := col + 1
+	sums[col] = 1
+	for i := 2; i < len(lines)-1; i += 2 {
+		for j := col; j < end; j++ {
+			if lines[i][j] == '^' && sums[j] > 0 {
+				sum++
+				sums[j-1] += sums[j]
+				sums[j+1] += sums[j]
+				sums[j] = 0
 			}
 		}
+		col -= 1
+		end += 1
 	}
-	for _, val := range sums[len(sums)-1] {
+	for _, val := range sums {
 		sum2 += val
 	}
 	fmt.Println(sum, sum2)
